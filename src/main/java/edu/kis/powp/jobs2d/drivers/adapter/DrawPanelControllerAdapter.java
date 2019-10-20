b949915bc5955132b93bb4cb535b0817a094a93b
MarcinBpl <materacbr@gmail.com>
2019-10-20 22:17:35 +0200

    Add files via upload
    Zadanie 1

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawPanelControllerAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawPanelControllerAdapter.java
@@ -1,5 +1,8 @@
 package edu.kis.powp.jobs2d.drivers.adapter;
 
+import javax.swing.JPanel;
+
+import edu.kis.legacy.drawer.panel.DefaultDrawerFrame;
 import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
@@ -8,11 +11,17 @@ import edu.kis.powp.jobs2d.Job2dDriver;
 /**
  * driver adapter to drawer with several bugs.
  */
-public class MyAdapter extends DrawPanelController implements Job2dDriver {
+public class DrawPanelControllerAdapter extends DrawPanelController implements Job2dDriver {
 	private int startX = 0, startY = 0;
 
-	public MyAdapter() {
+	public DrawPanelControllerAdapter() {
 		super();
+	}
+	
+	public DrawPanelControllerAdapter(JPanel panel)
+	{
+		super();
+		initialize(panel);
 	}
 
 	@Override
